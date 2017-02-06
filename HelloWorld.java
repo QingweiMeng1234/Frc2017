c3cc7436850062759909fab24187c563ba14baa6
magiinbethleham <webermeng@sina.cn>
2017-02-06 17:53:57 -0500

    Weber
    This is a test

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package Command;
+
+/**
+ * Created by lenovo on 2017/2/6.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HEllo WOrld");
+    }
+}
